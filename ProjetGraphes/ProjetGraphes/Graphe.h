d6096b948de341a1dc85cc460a99197681ba0980
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2017-04-04 17:02:48 +0200

    Graphe.h

--- ProjetGraphes/ProjetGraphes/Graphe.h
+++ ProjetGraphes/ProjetGraphes/Graphe.h
@@ -3,9 +3,25 @@
 
 class CGraphe
 {
+private:
+
+	unsigned int uiGRAnbSommets;
+	unsigned int * pSOMGRAsommets;
+
 public:
 	CGraphe();
+	CGraphe(CGraphe & GRAobjet);
 	~CGraphe();
+
+	CGraphe & operator=(CGraphe & GRAobjet);
+
+	unsigned int GRAgetNbSommets() const;
+
+	CSommet & GRAajouterSommet(unsigned int uiNumero);
+	CSommet & GRAgetSommet(unsigned int uiNumero) const;
+	void GRAsupprimerSommet(CSommet & SOMobjet);
+	
+	void GRAafficher() const;
 };
 
 #endif
