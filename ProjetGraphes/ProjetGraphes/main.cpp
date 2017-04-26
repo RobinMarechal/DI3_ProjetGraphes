4f790cca9d2490f62f9dc538fd25ca100cacbd55
GouVsky <greg6-marco@hotmail.fr>
2017-04-26 17:48:46 +0200

    Modifications.

--- ProjetGraphes/ProjetGraphes/main.cpp
+++ ProjetGraphes/ProjetGraphes/main.cpp
@@ -23,16 +23,9 @@ void robin()
 {
 	CGraphe g;
 
-	CSommet * s1 = g.GRAajouterSommet(1);
-	CSommet * s2 = g.GRAajouterSommet(2);
-	CSommet * s3 = g.GRAajouterSommet(3);
-
-	s1->SOMajouterSuccesseur(s2);
-
-	cout << s1->SOMgetNbSuccesseurs() << endl;
-	cout << s1->SOMgetNbPredecesseurs() << endl;
-	cout << s2->SOMgetNbSuccesseurs() << endl;
-	cout << s2->SOMgetNbPredecesseurs() << endl;
+	g.GRAajouterSommet(1);
+	g.GRAajouterSommet(2);
+	g.GRAajouterSommet(3);
 
 	g.GRAdebug();
 }
